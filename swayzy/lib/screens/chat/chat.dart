ca4c916b04aa692ebc2dba1be913c084d48127f8
CTTAPKU <cttarkug@gmail.com>
2025-08-12 11:41:49 +0300

    home screen and chat screen button changes

--- swayzy/lib/screens/chat/chat.dart
+++ swayzy/lib/screens/chat/chat.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:swayzy/global_widgets/animated_button.dart';
 import 'package:swayzy/screens/chat/widgets/message_card.dart';
-
-import '../../constants/app_button_styles.dart';
 import '../../global_widgets/custom_app_bar.dart';
 import '../../l10n/app_localizations.dart';
 import 'mocks/customer_messages.mocks.dart';
 import 'mocks/performer_messages.mocks.dart';
+import 'models/message.dart';
 
 class Chat extends StatefulWidget {
   const Chat({super.key});
@@ -14,76 +14,68 @@ class Chat extends StatefulWidget {
   State<Chat> createState() => _ChatState();
 }
 
-enum ViewMode { asCustomer, asPerformer }
-
 class _ChatState extends State<Chat> {
-  ViewMode currentMode = ViewMode.asCustomer;
+  final PageController _pageController = PageController();
+  int _currentPageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
     final String titleText = localizations.chatTitle;
 
-    final messages =
-        currentMode == ViewMode.asCustomer
-            ? customerMessages
-            : performerMessages;
-
     return Scaffold(
       appBar: CustomAppBar(title: titleText),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      currentMode = ViewMode.asCustomer;
-                    });
-                  },
-                  style:
-                      currentMode == ViewMode.asCustomer
-                          ? AppButtonStyles.selectedButton
-                          : AppButtonStyles.unselectedButton,
-                  child: Text(localizations.customerButton),
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      currentMode = ViewMode.asPerformer;
-                    });
-                  },
-                  style:
-                      currentMode == ViewMode.asPerformer
-                          ? AppButtonStyles.selectedButton
-                          : AppButtonStyles.unselectedButton,
-                  child: Text(localizations.performerButton),
-                ),
-              ),
-            ],
+          AnimatedButtons<int>(
+            firstLabel: localizations.customerButton,
+            secondLabel: localizations.performerButton,
+            currentMode: _currentPageIndex,
+            leftMode: 0,
+            rightMode: 1,
+            onChanged: (index) {
+              setState(() => _currentPageIndex = index);
+              _pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
           ),
-          SizedBox(height: 5),
+          const SizedBox(height: 5),
           Expanded(
-            child: ListView.builder(
-              itemCount: messages.length,
-              itemBuilder: (context, index) {
-                final msg = messages[index];
-                return MessageCard(
-                  photoURL: msg.photoURL,
-                  userName: msg.userName,
-                  orderTitle: msg.orderTitle,
-                  lastMessageText: msg.lastMessageText,
-                  messageTime: msg.messageTime,
-                  isChecked: msg.isChecked,
-                );
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
               },
+              children: [
+                _buildMessageList(customerMessages),
+                _buildMessageList(performerMessages),
+              ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Widget _buildMessageList(List<Message> messages) {
+    return ListView.builder(
+      itemCount: messages.length,
+      itemBuilder: (context, index) {
+        final msg = messages[index];
+        return MessageCard(
+          photoURL: msg.photoURL,
+          userName: msg.userName,
+          orderTitle: msg.orderTitle,
+          lastMessageText: msg.lastMessageText,
+          messageTime: msg.messageTime,
+          isChecked: msg.isChecked,
+        );
+      },
     );
   }
 }
--- swayzy/lib/screens/home/home.dart
+++ swayzy/lib/screens/home/home.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:swayzy/screens/home/widgets/in_process_grid.dart';
 import 'package:swayzy/screens/home/widgets/in_search_grid.dart';
 
-import '../../constants/app_button_styles.dart';
+import '../../global_widgets/animated_button.dart';
 import '../../global_widgets/custom_app_bar.dart';
 import '../../l10n/app_localizations.dart';
 
@@ -15,71 +15,70 @@ class Home extends StatefulWidget {
   State<Home> createState() => _HomeState();
 }
 
-enum ViewMode { inSearch, inProcess }
-
 class _HomeState extends State<Home> {
-  ViewMode currentMode = ViewMode.inSearch;
   final FirebaseFirestore firestoreInstance = FirebaseFirestore.instance;
+  var user = FirebaseAuth.instance.currentUser!;
+  final PageController _pageController = PageController();
+  int _currentPageIndex = 0;
+  late Future<List<Map<String, dynamic>>> _ordersFuture;
+  late Future<List<Map<String, dynamic>>> _adsFuture;
+  String _userId = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _userId = user.uid;
+    _ordersFuture = getOrderData();
+    _adsFuture = getAdsForUser(_userId);
+  }
 
   @override
   Widget build(BuildContext context) {
     final localizations = AppLocalizations.of(context)!;
     final String titleText = localizations.homeTitle;
-    var user = FirebaseAuth.instance.currentUser;
-    String userId = '';
-
-    if (user != null) {
-      userId = user.uid;
-    } else {
-      CircularProgressIndicator();
-    }
 
     return Scaffold(
       appBar: CustomAppBar(title: titleText),
       body: Column(
         children: [
-          Row(
-            children: [
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      currentMode = ViewMode.inSearch;
-                    });
-                  },
-                  style:
-                      currentMode == ViewMode.inSearch
-                          ? AppButtonStyles.selectedButton
-                          : AppButtonStyles.unselectedButton,
-                  child: Text(localizations.searchButton),
-                ),
-              ),
-              Expanded(
-                child: ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      currentMode = ViewMode.inProcess;
-                    });
-                  },
-                  style:
-                      currentMode == ViewMode.inProcess
-                          ? AppButtonStyles.selectedButton
-                          : AppButtonStyles.unselectedButton,
-                  child: Text(localizations.processButton),
-                ),
-              ),
-            ],
+          AnimatedButtons<int>(
+            firstLabel: localizations.searchButton,
+            secondLabel: localizations.processButton,
+            currentMode: _currentPageIndex,
+            leftMode: 0,
+            rightMode: 1,
+            onChanged: (index) {
+              setState(() => _currentPageIndex = index);
+              _pageController.animateToPage(
+                index,
+                duration: const Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            },
           ),
           const SizedBox(height: 10),
           Expanded(
-            child: RefreshIndicator(
-              onRefresh: () async {
-                setState(() {});
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _currentPageIndex = index;
+                });
               },
-              child:
-                  currentMode == ViewMode.inSearch
-                      ? InSearchGrid(ordersFuture: getOrderData())
-                      : InProcessGrid(ordersFuture: getAdsForUser(userId)),
+              children: [
+                RefreshIndicator(
+                  onRefresh: () async {
+                    setState(() {_ordersFuture = getOrderData();});
+                  },
+                  child: InSearchGrid(ordersFuture: _ordersFuture),
+                ),
+                RefreshIndicator(
+                  onRefresh: () async {
+                    setState(() {_adsFuture = getAdsForUser(_userId);});
+                  },
+                  child: InProcessGrid(ordersFuture: _adsFuture),
+                ),
+              ],
             ),
           ),
         ],
@@ -88,12 +87,9 @@ class _HomeState extends State<Home> {
   }
 
   Future<List<Map<String, dynamic>>> getAdsForUser(String userId) async {
-    CollectionReference inProcessRef = FirebaseFirestore.instance.collection(
-      'inProcess',
-    );
+    CollectionReference inProcessRef = FirebaseFirestore.instance.collection('inProcess');
     CollectionReference adsRef = FirebaseFirestore.instance.collection('ads');
-    QuerySnapshot inProcessSnapshot =
-        await inProcessRef.where('userId', isEqualTo: userId).get();
+    QuerySnapshot inProcessSnapshot = await inProcessRef.where('userId', isEqualTo: userId).get();
     List<Map<String, dynamic>> adsData = [];
     for (var doc in inProcessSnapshot.docs) {
       String inProcessId = doc.id;
@@ -113,10 +109,7 @@ class _HomeState extends State<Home> {
 
   Future<List<Map<String, dynamic>>> getOrderData() async {
     QuerySnapshot querySnapshot =
-        await firestoreInstance
-            .collection('ads')
-            .orderBy('createdAt', descending: true)
-            .get();
+    await firestoreInstance.collection('ads').orderBy('createdAt', descending: true).get();
     return querySnapshot.docs.map((doc) {
       Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
       data['id'] = doc.id;
